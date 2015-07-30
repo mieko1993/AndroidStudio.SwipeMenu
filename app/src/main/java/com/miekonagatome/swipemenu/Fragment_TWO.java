0453f6a970d6843b47259483286f6db62ee0cedd
mieko nagatome <mieko_193@hotmail.com>
2015-07-30 17:13:28 -0500

    Initial commit

---
+++ app/src/main/java/com/miekonagatome/swipemenu/Fragment_TWO.java
@@ -0,0 +1,16 @@
+package com.miekonagatome.swipemenu;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_TWO extends Fragment {
+
+    View rootView;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fm_two, container, false);
+        return rootView;
+    }
+}
